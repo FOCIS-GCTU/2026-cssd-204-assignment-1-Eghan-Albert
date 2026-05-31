39bfef23a225b4a994e26f761151b54b68657e77
Eghan-Albert <2425404309@live.gctu.edu.gh>
2026-05-31 14:23:03 +0000

    Completed initials assignment

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+#File: initials.py
+#Description: Print out my initials.
+#Assignment Number: 1
+#
+#Name: Baffoe Albert Eghan
+#STUDENT ID: 2425404309
+#Email: 2425404309@live.gctu.edu.gh
+#Grader: Augustus Buckman
+#
+#On my honor, Baffoe Eghan Albert, this programming assignment is my own work 
+#and i have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+    # Prints my initials in large stylized letters
 
+    print()
+    print("...BAE")
+    print()
+
+    print("BBBBBBBBBBBB.........AAAAAAAAAAAA.........EEEEEEEEEEEE.........")
+    print("BB........BB.........AA........AA.........EE.................")
+    print("BB........BB.........AA........AA.........EE.................")
+    print("BBBBBBBBBBBB.........AA........AA.........EE.................")
+    print("BB........BB.........AAAAAAAAAAAA.........EEEEEEEEEEEE.......")
+    print("BB........BB.........AA........AA.........EE.................")
+    print("BB........BB.........AA........AA.........EE.................")
+    print("BB........BB.........AA........AA.........EE.................")
+    print("BBBBBBBBBBBB....**...AA........AA....**...EEEEEEEEEEEE....**.")
+    print("BBBBBBBBBBBB....**...AA........AA....**...EEEEEEEEEEEE....**.")
+
+    print()
 
 
 main()
